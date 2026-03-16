23f885ae8a801b624cd058f55881441fc7f5d8f9
Tal Peled <talp2002@cs.colman.ac.il>
2026-03-16 11:32:22 +0200

    chore: add pm2 production environment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  apps: [{
-    name: 'traveltales',
-    script: './dist/src/server.js',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
-}
+  apps: [
+    {
+      name: "traveltales",
+      script: "./dist/src/server.js",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
